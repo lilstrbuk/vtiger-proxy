20aaefe1c17a22328ed2e3bb1f3a292ee4078bd7
Luke Starbuck <lil.starbuck@gmail.com>
2025-01-21 16:38:25 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
-require("dotenv").config(); // Load environment variables
 
 const app = express();
 app.use(cors()); // Enable CORS
